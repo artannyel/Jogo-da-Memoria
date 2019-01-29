6757b80ae03ee3d3607100d929af79b5ec86d068
nexoZero <sigma.leonnard@gmail.com>
2019-01-29 16:13:43 -0300

    alterando controle jogo 1

--- src/controlejogo/ConjuntoBotoes.java
+++ src/controlejogo/ConjuntoBotoes.java
@@ -5,13 +5,35 @@ import java.util.List;
 
 public class ConjuntoBotoes {
     private List<MyBotao> listConjBotao;
+    private int refConjBotao;
+    private int posList;
+    
+    public ConjuntoBotoes(int posList, String urlIcon, int qtdPares ){
+        this.posList = posList;
+        MyBotao botao;
+            for(int i = 0; i < qtdPares; i++){
+                botao = new MyBotao(urlIcon, i);
+                this.listConjBotao.add(botao);
+            }
+    }
+    
+    public int getRefConjBotao() {
+        return refConjBotao;
+    }
+
+    public void setRefConjBotao(int refConjBotao) {
+        this.refConjBotao = refConjBotao;
+    }
     
     public ConjuntoBotoes(){
         listConjBotao = new ArrayList();
     }
-    
-    public void addBotao(MyBotao botao){
-        this.listConjBotao.add(botao);
+
+    private static class MyButton {
+
+        public MyButton() {
+        }
     }
+   
     
 }
--- src/janelas/PainelJogo.java
+++ src/janelas/PainelJogo.java
@@ -1,13 +1,50 @@
 package janelas;
 
+import controlejogo.ConjuntoBotoes;
+import controlejogo.MyBotao;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class PainelJogo extends JFrame{
+    private ActionListener acaoBotao;
+    private List<ConjuntoBotoes> listConjBotoes;
+    private int MAX_JOGADAS = 2;
+    private int jogadas;
     private JPanel painel;
+    private MyBotao botao1;
+    private MyBotao botao2;
     
-    public PainelJogo( int lado){
+    public PainelJogo( int pares){
         super("Jogo da Memoria");
+        int largJanela, altJanela;
+            if(pares == 6){
+                largJanela = 4;
+                altJanela = 3;
+            } else if(pares == 8){
+                largJanela = 4;
+                altJanela = 4;
+            }  else if(pares == 10){
+                largJanela = 5;
+                altJanela = 4;
+            }  else{
+                largJanela = 6;
+                altJanela = 4;
+            }
+        acaoBotao = new  ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                int posBotao;
+                posBotao = ((MyBotao) ae.getSource()).getNumBotao();
+                    for(ConjuntoBotoes conjBot : listConjBotoes){
+                        jogadas++;
+                    }
+            }
+            
+        };
         
         //Configurando o painel
         this.painel = new JPanel();
@@ -15,8 +52,13 @@ public class PainelJogo extends JFrame{
         this.painel.setLayout(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        this.setSize(defTamPainel(lado), defTamPainel(lado)); // definir o tamanho da janela PainelJogo
+        this.setSize(defTamPainel(largJanela), defTamPainel(altJanela)); // definir o tamanho da janela PainelJogo
         this.setLocationRelativeTo(null); //Centralizar a PainelJogo no meio
+        
+       botao1 = new MyBotao("");
+       botao1.setBounds(10, 10, 64, 64);
+       botao1.setBackground(null);
+       this.painel.add(this.botao1);
         
         this.painel.setVisible(true); // definir visibilidade dessa janela
         this.setResizable(false); // impedir que o tamanho original mude
@@ -25,7 +67,7 @@ public class PainelJogo extends JFrame{
     }
     private int defTamPainel( int Lado){
         int tamanho;
-        tamanho = ((Lado+1)*5)+(Lado*50);
+        tamanho = ((Lado+1)*10)+(Lado*64);
         System.out.println(tamanho);
         return tamanho;
     }
